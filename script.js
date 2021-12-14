d6973ab9171401eefa5b2d437db956f29572ed79
schalsma <90012386+schalsma@users.noreply.github.com>
2021-12-13 20:59:30 -0500

    login

--- script.js
+++ script.js
@@ -29,3 +29,17 @@ function onSignIn(googleUser) {
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   }
+
+  function renderButton() {
+    gapi.signin2.render('my-signin2', {
+      'scope': 'profile email',
+      'width': 240,
+      'height': 50,
+      'longtitle': true,
+      'theme': 'dark',
+      'ux_mode': 'redirect',
+      'redirect_uri': 'index.html',
+      'onsuccess': onSuccess,
+      'onfailure': onFailure
+    });
+  }
