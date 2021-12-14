f3520d878f4cc87b25a2f331bc62fedb6cd4f3e4
schalsma <90012386+schalsma@users.noreply.github.com>
2021-12-14 16:14:50 -0500

    calendar

--- calendar.js
+++ calendar.js
@@ -46,4 +46,30 @@ function handleAuthClick(event) {
     return false;
 }
 
-/* End of PART 1 - Authentication Process. */   
+/* End of PART 1 - Authentication Process. */  
+
+// Make an API call to create an event.  Give feedback to user.
+function createEvent(eventData) {
+  // First create resource that will be send to server.
+    var resource = {
+        "summary": eventData.eventTitle,
+        "start": {
+          "dateTime": new Date(eventData.date + " " + eventData.startTime).toISOString()
+        },
+        "end": {
+          "dateTime": new Date(eventData.date + " " + eventData.endTime).toISOString()
+          }
+        };
+    // create the request
+    var request = gapi.client.calendar.events.insert({
+      'calendarId': 'primary',
+      'resource': resource
+    });
+  
+    // execute the request and do something with response
+    request.execute(function(resp) {
+      console.log(resp);
+      alert("Your event was added to the calendar.");
+    });
+  }
+         
