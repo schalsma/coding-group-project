45d759df95b41349c96f14bf98818fec8916787c
schalsma <90012386+schalsma@users.noreply.github.com>
2021-12-14 16:05:04 -0500

    calendar

--- calendar.js
+++ calendar.js
@@ -1,120 +1,49 @@
-// Client ID and API key from the Developer Console
-var CLIENT_ID = "903874887931-depq8q79cg0n8gk6sajkdtn2qee3m9hk.apps.googleusercontent.com";
-var API_KEY = "AIzaSyCE-mX4Xwe_E6cXd5c_17IlRcFJlXjYPng";
+// PART 1 - Authorization and login.
 
-// Array of API discovery doc URLs for APIs used by the quickstart
-var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
-
-// Authorization scopes required by the API; multiple scopes can be
-// included, separated by spaces.
-var SCOPES =  "https://www.googleapis.com/auth/calendar.events https://www.googleapis.com/auth/calendar.events.readonly https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.readonly https://www.googleapis.com/auth/calendar.settings.readonly";
-
-var authorizeButton = document.getElementById("authorize_button");
-var signoutButton = document.getElementById("signout_button");
-
-/**
- *  On load, called to load the auth2 library and API client library.
- */
+// Global variables, the values come from the Developer Console
+// Put your OWN clientID and apiKey
+var clientId = '903874887931-depq8q79cg0n8gk6sajkdtn2qee3m9hk.apps.googleusercontent.com';
+var apiKey = 'AIzaSyCE-mX4Xwe_E6cXd5c_17IlRcFJlXjYPng';
+var scopes = 'https://www.googleapis.com/auth/calendar';
+      
+      
+/* Function invoked when the client javascript library is loaded */
 function handleClientLoad() {
-  gapi.load("client:auth2", initClient);
+  console.log("Inside handleClientLoad ...");
+  gapi.client.setApiKey(apiKey);
+  window.setTimeout(checkAuth,100);
 }
 
-/**
- *  Initializes the API client library and sets up sign-in state
- *  listeners.
- */
-function initClient() {
-  gapi.client.init({
-    apiKey: API_KEY,
-    clientId: CLIENT_ID,
-    discoveryDocs: DISCOVERY_DOCS,
-    scope: SCOPES
-  }).then(function () {
-    // Listen for sign-in state changes.
-    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-    // Handle the initial sign-in state.
-    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-    authorizeButton.onclick = handleAuthClick;
-    signoutButton.onclick = handleSignoutClick;
-  }, function(error) {
-    appendPre(JSON.stringify(error, null, 2));
-  });
+/* API function to check whether the app is authorized. */
+function checkAuth() {
+  console.log("Inside checkAuth ...");
+  gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true}, 
+                      handleAuthResult);
 }
 
-/**
- *  Called when the signed in status changes, to update the UI
- *  appropriately. After a sign-in, the API is called.
- */
-function updateSigninStatus(isSignedIn) {
-  if (isSignedIn) {
-    authorizeButton.style.display = "none";
-    signoutButton.style.display = "block";
-    listUpcomingEvents();
-  } else {
-    authorizeButton.style.display = "block";
-    signoutButton.style.display = "none";
-  }
-}
-
-/**
- *  Sign in the user upon button click.
- */
-function handleAuthClick(event) {
-  gapi.auth2.getAuthInstance().signIn();
-}
-
-/**
- *  Sign out the user upon button click.
- */
-function handleSignoutClick(event) {
-  gapi.auth2.getAuthInstance().signOut();
-}
-
-/**
- * Append a pre element to the body containing the given message
- * as its text node. Used to display the results of the API call.
- *
- * @param {string} message Text to be placed in pre element.
- */
-function appendPre(message) {
-  var pre = document.getElementById('content');
-  var textContent = document.createTextNode(message + '\n');
-  pre.appendChild(textContent);
-}
-
-/**
- * Print the summary and start datetime/date of the next ten events in
- * the authorized user's calendar. If no events are found an
- * appropriate message is printed.
- */
-function listUpcomingEvents() {
-  gapi.client.calendar.events.list({
-    'calendarId': 'primary',
-    'timeMin': (new Date()).toISOString(),
-    'showDeleted': false,
-    'singleEvents': true,
-    'maxResults': 10,
-    'orderBy': 'startTime'
-  }).then(function(response) {
-    var events = response.result.items;
-    appendPre('Upcoming events:');
-
-    if (events.length > 0) {
-      for (i = 0; i < events.length; i++) {
-        var event = events[i];
-        var when = event.start.dateTime;
-        if (!when) {
-          when = event.start.date;
-        }
-        appendPre(event.summary + ' (' + when + ')')
-      }
+/* Invoked by different functions to handle the result of authentication checks.*/
+function handleAuthResult(authResult) {
+    console.log("Inside handleAuthResult ...");
+    var authorizeButton = document.getElementById('authorize-button');
+    var addButton = document.getElementById('addToCalendar');
+    if (authResult && !authResult.error) {
+          authorizeButton.style.visibility = 'hidden';
+          addButton.style.visibility = 'visible'; 
+          //load the calendar client library
+          gapi.client.load('calendar', 'v3', function(){ 
+            console.log("Calendar library loaded.");
+          });
     } else {
-      appendPre('No upcoming events found.');
-    }
-  });
+          authorizeButton.style.visibility = '';
+          authorizeButton.onclick = handleAuthClick;
+        }
 }
 
-// async defer src="https://apis.google.com/js/api.js"
-// onload="this.onload=function(){};handleClientLoad()"
-// onreadystatechange="if (this.readyState === 'complete') this.onload()">
+/* Event handler that deals with clicking on the Authorize button.*/
+function handleAuthClick(event) {
+    gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, 
+                        handleAuthResult);
+    return false;
+}
+
+/* End of PART 1 - Authentication Process. */   
