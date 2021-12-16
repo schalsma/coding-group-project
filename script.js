347af17ea64635ccf0c7ac8d2221c3ec05e51c70
brittyc <bc3290@nyu.edu>
2021-12-16 10:56:37 -0500

    pop up

--- script.js
+++ script.js
@@ -13,40 +13,25 @@ const assignment_txt = <document.querySelector("#assignment_txt");
     setData_btn.addEventListener("click", event => {
         localStorage.setItem("myAssignment", assignment_txt.value);
         location.href = "getLms.html";
-    }) 
+    }); 
 
-// Get the modal
-var modal = document.getElementById("#myModal");
+let modalBtn = document.getElementById("#modal-btn");
+ let modal = document.querySelector(".modal");
+let closeBtn = document.querySelector(".close-btn");
+modalBtn.onclick = function()
+{
+modal.style.display = "block";
+    }
+closeBtn.onclick = function(){
+      modal.style.display = "none";
+    }
+window.onclick = function(e){
+      if(e.target == modal){
+        modal.style.display = "none";
+      }
+    }
 
-// Get the button that opens the modal
-var btn = document.getElementById("#myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName(".close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
     
-/* popup 
-const openModalBtn = document.querySelector("#openModalBtn");
-openModalBtn.addEventListener("click",event =>{
-    
-})
-*/
 
 // Google Stuff
 function onSignIn(googleUser) {
