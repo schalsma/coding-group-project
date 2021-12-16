15cc2f1b2f9a0776ef67a837484518126cf9f356
schalsma <90012386+schalsma@users.noreply.github.com>
2021-12-16 11:25:23 -0500

    changes

--- script.js
+++ script.js
@@ -1,37 +1,29 @@
-/*  
-const localStorageAssignment = localStorage.getItem("myAssignment");
-    
-if(localStorageAssignment == "apple"){
-        console.log("You may enter");
-    }else{
-        console.log("Goodbye")
-    }
-*/
-const letData_btn = <document.querySelector("#setData_btn");
-const assignment_txt = <document.querySelector("#assignment_txt");
+// Get the modal
+var modal = document.getElementById("myModal");
 
-    setData_btn.addEventListener("click", event => {
-        localStorage.setItem("myAssignment", assignment_txt.value);
-        location.href = "getLms.html";
-    }); 
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-let modalBtn = document.getElementById("#modal-btn");
- let modal = document.querySelector(".modal");
-let closeBtn = document.querySelector(".close-btn");
-modalBtn.onclick = function()
-{
-modal.style.display = "block";
-    }
-closeBtn.onclick = function(){
-      modal.style.display = "none";
-    }
-window.onclick = function(e){
-      if(e.target == modal){
-        modal.style.display = "none";
-      }
-    }
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-    
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 
 // Google Stuff
 function onSignIn(googleUser) {
@@ -43,3 +35,4 @@ function onSignIn(googleUser) {
     console.log('Family Name: ' + profile.getFamilyName());
     console.log("Image URL: " + profile.getImageUrl());
     console.log("Email: " + profile.getEmail());
+}
