f8f9b6b7d30acea80a46a13ee8364b432a57fd5c
brittyc <bc3290@nyu.edu>
2021-12-16 10:41:41 -0500

    pop up

--- script.js
+++ script.js
@@ -14,12 +14,39 @@ const assignment_txt = <document.querySelector("#assignment_txt");
         localStorage.setItem("myAssignment", assignment_txt.value);
         location.href = "getLms.html";
     }) 
+
+// Get the modal
+var modal = document.getElementById("#myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("#myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName(".close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
     
-/* popup */
+/* popup 
 const openModalBtn = document.querySelector("#openModalBtn");
 openModalBtn.addEventListener("click",event =>{
     
 })
+*/
 
 // Google Stuff
 function onSignIn(googleUser) {
